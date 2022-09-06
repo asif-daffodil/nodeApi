aaa4ce475e9dda8ac9117a4d01f9ff38e10f4910
Asif Abir <abir@dipti.com.bd>
2022-09-06 20:54:20 +0600

    install nodemon and view template

--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,6 @@
 const api = require("../api/api.json");
 const getRandomUser = (req, res) => {
-  res.send("harami");
+  res.send(api[Math.floor(Math.random() * api.length)]);
 };
 
 module.exports = { getRandomUser };
--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const express = require("express");
 const app = express();
 const userRouter = require("./routes/userRouter");
+app.use("/", userRouter);
+app.use("*", (req, res) => {
+  res.send("No route found");
+});
+app.listen(5000, () => {});
